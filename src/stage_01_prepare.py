821af53d3c6bf9d98ad129a084bdaf87c2cc4919
shivpalSW <shivpal.wadkar2050@gmail.com>
2023-07-13 17:36:44 +0530

    stage -01 done

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -1,14 +1,14 @@
 import argparse
 import os
+import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml,create_directories
-import json
-import shutil
+from src.utils.common import read_yaml, create_directories
+from src.utils.data_mgmt import process_posts
 import random
 
 
-STAGE = "ONE" ## <<< change stage name 
+STAGE = "One"
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -17,42 +17,34 @@ logging.basicConfig(
     filemode="a"
     )
 
-
 def main(config_path, params_path):
-    ## converting XML data to tsv format
+    ## converting XML data tsv
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
 
-    source_data= config["source_data"]
-    input_data= os.path.join(source_data["data_dir"],source_data["data_file"])
+    source_data = config["source_data"]
+    input_data = os.path.join(source_data["data_dir"], source_data["data_file"])
 
-    split= params["prepare"]["split"]
+    split = params["prepare"]["split"]
     seed = params["prepare"]["seed"]
+
     random.seed(seed)
 
-    artifacts= config["artifacts"]
-    prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
+    create_directories([prepared_data_dir_path])
 
-    create_directories([prepare_data_dir_path])
-
-    train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
-    test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
-
-    encoding='utf8'
-    with open (input_data,encoding=encoding) as fd_in :
-        with open(train_data_path,"w",encoding= encoding) as fd_out_train :
-            with open (test_data_path,"w",encoding= encoding) as fd_out_test:
-                pass
-                #processed_posts(fd_in,fd_out_train,fd_out_test,"<python>",split)]
-
+    train_data_path = os.path.join(prepared_data_dir_path, artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepared_data_dir_path, artifacts["TEST_DATA"])
+    
+    encode = "utf8"
+    with open(input_data, encoding=encode) as fd_in:
+        with open(train_data_path, "w", encoding=encode) as fd_out_train:
+            with open(test_data_path, "w", encoding=encode) as fd_out_test:
+                process_posts(fd_in, fd_out_train, fd_out_test, "<python>", split)
     
     
                                   
-
-
-
-
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
@@ -63,7 +55,7 @@ if __name__ == '__main__':
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
         main(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info(f">>>>>>> stage {STAGE} completed!<<<<<<<<<\n")
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
         raise e
