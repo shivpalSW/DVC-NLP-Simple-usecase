cee8e82759d82a4680532d54719cf617deba0077
shivpalSW <shivpal.wadkar2050@gmail.com>
2023-07-16 14:26:10 +0530

    stage -02 train data tfidf matrix saved

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -4,7 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories, get_df
-#from src.utils.featurize import save_matrix
+from src.utils.featurize import save_matrix
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
@@ -39,7 +39,23 @@ def main(config_path, params_path):
     df_train = get_df(train_data_path)
 
     train_words = np.array(df_train.text.str.lower().values.astype("U")) ## << U1000
-    print(train_words[: 5])
+    # print(train_words[: 5])
+
+    bag_of_words = CountVectorizer(
+        stop_words="enlish",
+        max_features= max_features,
+        ngram_range=(1,ngrams)
+        )
+
+    bag_of_words.fit(train_words)
+    train_words_binary_matrix = bag_of_words..transform(train_words)
+
+    tfidf = TfidfTransformer(smooth_idf=False)
+    tfidf.fit(train_words_binary_matrix)
+    train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
+    save_matrix(df_train, train_words_tfidf_matrix, featurized_train_data_path)  
+
+
 
     
 
